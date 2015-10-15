caeea23c4fe8f9119d0a982a0c94f2483e38ce7d
Denis Savenok <denissavenok@gmail.com>
2015-10-16 01:06:22 +0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { runNode, all, coroutine } from 'creed';
+import { readFile } from 'fs';
+import { join } from 'path';
+
+// joinPath :: String -> String -> String
+const joinPath = init => tail => join(init, tail);
+
+// readFileP :: String -> String -> Promise Error Buffer
+const readFileP = encoding => file => runNode(readFile, file, {encoding});
+
+// pipe :: (a -> b) -> (b -> c) -> (a -> c)
+const pipe = (f, g) => x => g(f(x));
+
+// concatFiles :: String -> Promise Error String
+const concatFiles = coroutine(function* (dir) {
+    const readUtf8P = pipe(joinPath(dir), readFileP('utf8'));
+
+    const index = yield readUtf8P('index.txt');
+    const results = yield all(index.match(/^.*(?=\n)/gm).map(readUtf8P));
+    return results.join('');
+});
+
+const main = process => 
+    console.log("process.argv::::", process.argv);
+    concatFiles(process.argv[2])
+    .then(s => process.stdout.write("RESULT:::: " + s + "\n"));
+
+main(process);
